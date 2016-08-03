6a32c49c4c1777dddb583d801da4c6ccb61f97ef
StaphSynth <ihatemacs@gmail.com>
2016-08-03 23:49:19 +1000

    Styling/responsive css
    Forced footer to stay down. Fixed footer responsive issues.
    
    Much response css for tablets, desktops largely still to do.
    
    Added some placeholder text to common.html for testing as I don't have a
    Jekyll server on this machine to allow addition of proper content. Will
    remove later.

--- js/synth.js
+++ js/synth.js
@@ -3,5 +3,13 @@ $("#navIcon").click(function(event){
   $("#mainMenu ul").toggle();
 });
 
+/*Remove inline style from #mainMenu ul if window width > 800px on window resize event.
+Prevents inline style from over-riding CSS style rules in @media query*/
+$(window).resize(function() {
+  if($(window).width() >= 800) {
+    //if($("#mainMenu ul").attr("style") === "display: none;" || $("#mainMenu ul").attr("style") === "display: block;")
+        $("#mainMenu ul").removeAttr("style");
+  }
+});//end window resize event
 
 console.log("synth.js has loaded and run");
