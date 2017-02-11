9f72a93f9a1e00bf677d5f013cb44ce0b5015e18
Staphsynth <1337dallen@gmail.com>
2017-02-11 12:14:14 +1100

    add angular post and calc app

---
+++ _posts/2017-02-10-Learning-angular.md
@@ -0,0 +1,30 @@
+---
+layout: post
+title: Learning AngularJS
+imageSource: /img/angularjs-logo.jpg
+byline: Includes single-page calculator app! :0
+category: coding
+tags:
+  - javascript
+  - development
+---
+
+Most of my time with JavaScript has been spent with vanilla JS or jQuery. Everyone seems to be jumping onto a variety of other JS bandwagons, though, so I thought I'd start playing with some of them as well.
+
+I've been learning AngularJS, and decided to write some simple single-page apps to quickly get to grips with the new paradigm. Presented here is my first one: a (very) simple calculator.
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.32/angular.min.js"></script>
+<script type="text/javascript" src="http://127.0.0.1:4000/js/calc.js"></script>
+<link type="text/css" rel="stylesheet" href="http://127.0.0.1:4000/css/calc.css">
+{% raw %}
+<div class="calc" ng-app="calc-app">
+  <div ng-controller="calc-controller">
+    <table>
+      <tbody>
+        <tr><td colspan="4" align="right" class="display">{{display}}</td></tr>
+        <tr ng-repeat="row in buttons"><td ng-repeat="digit in row"><button class="calcButton" ng-click="push(digit)">{{digit}}</button></td></tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+{% endraw %}
---
+++ js/calc.js
@@ -0,0 +1,67 @@
+var calcApp = angular.module('calc-app', []);
+
+calcApp.controller('calc-controller', function($scope){
+  //globals
+  $scope.display = 0;
+  $scope.operand = 0;
+  $scope.decPoint = false;
+  $scope.postDecPresses = 0;
+  $scope.operator = '';
+  $scope.buttons = [
+    [7,8,9,'+'],
+    [4,5,6,'-'],
+    [1,2,3,'/'],
+    ['.',0,'=','*']
+  ];
+  //sets up operator conditions when operator selected
+  $scope.operation = function(operator) {
+    $scope.operator = operator;
+    $scope.operand = $scope.display;
+    $scope.display = 0;
+    $scope.decPoint = false;
+    $scope.postDecPresses = 0;
+  }; //operation
+
+  //resolves the equation
+  $scope.resolve = function() {
+    if($scope.operator === '')
+      return
+    else if($scope.operator === '+')
+      $scope.display += $scope.operand;
+    else if ($scope.operator === '-')
+      $scope.display = $scope.operand - $scope.display;
+    else if($scope.operator === '*')
+      $scope.display *= $scope.operand;
+    else if($scope.operator === '/')
+        $scope.display = $scope.operand / $scope.display;
+    //reset globals and flags after resolution
+    $scope.operator = '';
+    $scope.operand = 0;
+    $scope.decPoint = false;
+    $scope.postDecPresses = 0;
+  }; //resolve
+
+  //called when the user pushes a button on the calc
+  $scope.push = function(value) {
+    //is value a number?
+    if(!isNaN(parseFloat(value))) {
+      if($scope.decPoint === false) {
+        $scope.display = ($scope.display * 10) + value;
+      } else { //if decimal point pressed
+        $scope.postDecPresses++;
+        $scope.display += value * Math.pow(10, ($scope.postDecPresses * -1));
+      }
+    } else { //if value is an operation, do maths logic
+      if((value === '+') || (value === '-') || (value === '*') || (value === '/')) {
+        $scope.operation(value);
+      //special cases
+      } else if(value === '=') {
+        $scope.resolve()
+      } else if(value === '.') {
+        $scope.decPoint = true;
+      }
+    }
+  }; //push
+
+
+}); //controller
