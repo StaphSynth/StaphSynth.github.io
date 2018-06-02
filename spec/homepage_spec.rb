d8dfa3873fe6b9ad332e5f4c721351c3f9f34d31
David Allen <dallen@zendesk.com>
2018-06-02 10:30:06 +1000

    Add tests for homepage

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -3,7 +3,17 @@ require 'spec_helper'
 describe 'home page', type: :feature, js: true do
   before { visit '/' }
 
+  it 'has the site heading' do
+    expect(find('#heading').text).to eq("Synthetase's World of Nerd")
+  end
+
   it 'has the welcome message' do
     expect(page).to have_text('Welcome')
+  end
+
+  describe 'post list' do
+    it 'has latest posts' do
+      expect(find_all('.articleListItem').count).to eq 2
+    end
   end
 end
--- spec/layouts/common_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'common layout', type: :feature, js: true do
-  before { visit '/' }
-
-  it 'has the site heading' do
-    expect(find('#heading').text).to eq("Synthetase's World of Nerd")
-  end
-end
