9f20e89db6fb36662ce2555db66580be235c4ca4
Staphsynth <1337dallen@gmail.com>
2018-08-25 22:35:18 +1000

    Tidy and update JS

--- js/synth.js
+++ js/synth.js
@@ -1,27 +1,27 @@
 /*show/hide the main menu when nav icon clicked/tapped on mobile view
 also blurs the button after event and rotates the menu direction caret*/
-$("#navIcon").click(function(event){
-  $("#mainMenu ul").slideToggle("fast","swing");
-  $("#navIcon").blur();
-  $(".fa-caret-down").toggleClass("fa-rotate-180");
+$('#navIcon').click(function(event) {
+  $('#mainMenu ul').slideToggle('fast','swing');
+  $('#navIcon').blur();
+  $('.fa-caret-down').toggleClass('fa-rotate-180');
 });
 
 /*For figure captions: centre the caption if there's only one line of text, otherwise leave it justified.
 Calc: get the container height and divide by line hieght. If the answer is 1, then there's only 1 line*/
-$(".caption").each(function(){
+$('.caption').each(function(){
   var height = $(this).height();
-  var lineHeight = $(this).css("line-height");
+  var lineHeight = $(this).css('line-height');
   lineHeight = parseFloat(lineHeight);
   if(Math.round(height / lineHeight) < 2)
-    $(this).css("text-align", "center");
+    $(this).css('text-align', 'center');
 });
 
 /*Remove inline style from #mainMenu ul if window width > 800px on window resize event.
 Prevents inline style from over-riding CSS style rules in @media query*/
 $(window).resize(function() {
   if($(window).width() >= 800) {
-    $("#mainMenu ul").removeAttr("style");
+    $('#mainMenu ul').removeAttr('style');
   }
-});//end window resize event
+});
 
-//console.log("synth.js has loaded and run");
+//console.log('synth.js has loaded and run');
